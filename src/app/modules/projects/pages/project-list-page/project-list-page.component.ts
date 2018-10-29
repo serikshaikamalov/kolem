8bc5fb6a7ba112547a65d5d83f4bd99ef82d1a44
Serik Shaikamalov <serik.shaikamalov@nt.local>
2018-10-29 17:42:05 +0600

    projects list: added logics

--- src/app/modules/projects/pages/project-list-page/project-list-page.component.ts
+++ src/app/modules/projects/pages/project-list-page/project-list-page.component.ts
@@ -6,7 +6,92 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./project-list-page.component.scss']
 })
 export class ProjectListPageComponent implements OnInit {
+    
+    public typeId: number = 0;
+    public projectTypes: any[] = [];
+    public projects: any[] = [];
+    public allProjects: any[] = [];
+    
     constructor() { }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {        
+        this.loadMenu();
+        this.loadData();
+    }
+
+
+
+    /**
+     * Menu
+     */
+    public loadMenu(){
+        this.projectTypes.push(
+            {                
+                ProjectTypeId: 0,
+                Title: 'Все'
+            },
+            {                
+                ProjectTypeId: 1,
+                Title: 'Архитектура'
+            },
+            {                
+                ProjectTypeId: 2,
+                Title: 'Интерьер'
+            },
+            {                
+                ProjectTypeId: 3,
+                Title: 'Визуализация'
+            }
+        );
+    }
+
+
+    /**
+     * Projects
+     */
+    public loadData(){
+        this.projects.push(
+            {
+                Id: 1,
+                Title: 'ДИЗАЙН ИНТЕРЬЕРА 1-КОМНАТНОЙ КВАРТИРЫ АЛМАТЫ, КАЗАХСТАН',
+                TypeId: 1,
+                ImageSrc: '../assets/images/interier_01.png'
+            },
+            {
+                Id: 2,
+                Title: 'РЕКОНСТРУКЦИЯ ФАСАДА ЗДАНИЙ ГОРОДСКОГО СУДА Г. АЛМАТЫ',
+                TypeId: 2,
+                ImageSrc: '../assets/images/judge_center.png'
+            },
+            {
+                Id: 1,
+                Title: 'ДИЗАЙН ИНТЕРЬЕРА 1-КОМНАТНОЙ КВАРТИРЫ АЛМАТЫ, КАЗАХСТАН',
+                TypeId: 3,
+            },
+            {
+                Id: 1,
+                Title: 'ДИЗАЙН ИНТЕРЬЕРА 1-КОМНАТНОЙ КВАРТИРЫ АЛМАТЫ, КАЗАХСТАН',
+                TypeId: 2,
+            },
+        );
+
+        this.allProjects = this.projects;        
+    }
+
+
+    /**
+     * FILTER
+     */
+    public filterByProjetType( typeId: number = 0 ){     
+        this.typeId = typeId;   
+
+        if( typeId ){
+            this.projects = this.allProjects.filter( x => x.TypeId == typeId );
+
+        }else{
+            this.projects = this.allProjects;
+        }
+
+    }
+
 }
--- src/app/modules/projects/projects.module.ts
+++ src/app/modules/projects/projects.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectListPageComponent } from './pages/project-list-page/project-list-page.component';
 import { ProjectDetailsPageComponent } from './pages/project-detail-page/project-details-page.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   imports: [
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
+    CommonModule
   ],
   declarations: [
     ProjectListPageComponent,
