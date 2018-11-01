84964c6fc181e21414125f831a42288035d561f6
Serik Shaikamalov <serik.shaikamalov@nt.local>
2018-11-01 15:59:42 +0600

    Added Contact, about, services pages

--- src/app/modules/contacts/pages/contacts-page/contacts-page.component.ts
+++ src/app/modules/contacts/pages/contacts-page/contacts-page.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var DG;
+
 @Component({
     selector: 'contacts-page',
     templateUrl: './contacts-page.component.html',
     styleUrls: ['./contacts-page.component.scss']
 })
+
 export class ContactsIndexPageComponent implements OnInit {
     constructor() { }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {                 
+
+    }
+
+
+    ngAfterViewInit(): void {
+        //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+        //Add 'implements AfterViewInit' to the class.
+
+        let map;
+
+        DG.then(function () {
+            map = DG.map('map', {
+                center: [43.263154, 76.940571],
+                zoom: 17
+            }
+            );
+
+            DG.marker([43.263154, 76.940571]).addTo(map).bindPopup('Kolem Architecture!');
+        });
+        
+    }
 }
